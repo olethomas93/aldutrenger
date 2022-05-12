111ec0adf7217392e24c26e1b83f5df9f023573d
olethomas93 <norbye93@gmail.com>
2022-05-12 19:23:57 +0200

    xsx

--- vue.config.js
+++ vue.config.js
@@ -3,20 +3,10 @@ module.exports = defineConfig({
   transpileDependencies: true,
 
   pwa: {
-    name: 'My App',
-    themeColor: '#4DBA87',
-    msTileColor: '#000000',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-
-    // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'dev/sw.js',
+   
       exclude: [/_redirects/]
       
       // ...other Workbox options...
-    }
+    
   }
 });
