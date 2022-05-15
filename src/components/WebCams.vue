ddae3375ec9979d41254dcc14707e864fba21df5
olethomas93 <norbye93@gmail.com>
2022-05-15 22:10:20 +0200

    dsd

--- src/components/WebCams.vue
+++ src/components/WebCams.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
+
+<div class="container-fluid">
+<div class="row">
+  <div class="col">
     <img
       src="https://camera.strandafjellet.no:8447/axis-cgi/mjpg/video.cgi?resolution=800x450&amp;dummy=1652445497027"
       height="450"
@@ -17,4 +20,48 @@
       "
     />
   </div>
+
+</div>
+<div class="row">
+  <div class="col">
+    <img
+      src="https://camera.strandafjellet.no:8442/axis-cgi/mjpg/video.cgi?resolution=800x450&dummy=1652645335212"
+      height="450"
+      width="800"
+      alt="Camera Image (8442)"
+      style="
+        height: 100%;
+        left: 0;
+        object-fit: cover;
+
+        top: 0;
+        width: 100%;
+      "
+    />
+  </div>
+
+</div>
+<div class="row">
+  <div class="col">
+    <img
+      src="https://camera.strandafjellet.no:8444/axis-cgi/mjpg/video.cgi?resolution=800x450&dummy=1652645335213"
+      height="450"
+      width="800"
+      alt="Camera Image (8444)"
+      style="
+        height: 100%;
+        left: 0;
+        object-fit: cover;
+
+        top: 0;
+        width: 100%;
+      "
+    />
+  </div>
+
+</div>
+
+
+</div>
+
 </template>
